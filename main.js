df0d1d232642fda20e874e9f7232dba3f214861d
JALLARAPUSAIKUMAR <127927015+JALLARAPUSAIKUMAR@users.noreply.github.com>
2023-09-27 17:49:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var varValue;
+let letvalue;
+
+function varValue(){
+    varValue="100";
+    document.getElementById("varValue").textContent=varValue;
+    
+}
+
+function letValue(){
+    letValue="200";
+    document.getElementById("letValue").textContent=letValue;
+    
+}
+function constValue(){
+    constValue="300";
+    document.getElementById("constValue").textContent=constValue;
+    
+}
+
+
